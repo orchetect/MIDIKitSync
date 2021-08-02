1297e88fd3638e673fb139c5c156079ded58d3b6
Steffan Andrews <stef@orchetect.com>
2021-08-02 12:56:40 -0700

    Preflight release using MIDIKit 0.1.7

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/orchetect/MIDIKit", .branch("dev")), // from: "0.2.0"),
+        .package(url: "https://github.com/orchetect/MIDIKit", .exact("0.1.7")),
         .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.2.3")
     ],
 
