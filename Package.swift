1ec83f79134ba159cbcf2712ee954f8eb7168e0c
Steffan Andrews <stef@orchetect.com>
2021-12-02 04:00:34 -0800

    Bumped MIDIKit to 0.2.5

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/orchetect/MIDIKit", from: "0.2.1"),
+        .package(url: "https://github.com/orchetect/MIDIKit", from: "0.2.5"),
         .package(url: "https://github.com/orchetect/TimecodeKit", from: "1.2.6")
     ],
 
